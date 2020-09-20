c2990a24b977b92fcceebe646b513b2667490fad
sivasankaranm <sivazeee@yahoo.co.in>
2020-09-20 10:36:00 +0530

    6 files added

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+
+const routes: Routes = [
+  {path:'',redirectTo:'header',pathMatch:'full'},
+  {path:'header',component:HeaderComponent},
+  {path:'footer',component:FooterComponent},
+  {path:'product',component:ProductComponent},
+  {path:'productlist',component:ProductListComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
