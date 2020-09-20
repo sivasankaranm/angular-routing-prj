9aa4526dfd5c493a132425414c02bdf04a60cb54
sivasankaranm <sivazeee@yahoo.co.in>
2020-09-20 10:42:00 +0530

    2 files modified, 16 added, 16 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProductComponent } from './product/product.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { HeaderComponent } from './header.component';
+import { FooterComponent } from './footer.component';
+import { ProductComponent } from './product.component';
+import { ProductListComponent } from './product-list.component';
+import { PageNotFoundComponent } from './page-not-found.component'
 
 const routes: Routes = [
   {path:'',redirectTo:'header',pathMatch:'full'},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ProductComponent } from './product/product.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header.component';
+import { FooterComponent } from './footer.component';
+import { ProductComponent } from './product.component';
+import { ProductListComponent } from './product-list.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
